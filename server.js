8d4a7ab8ff7828d5d50300a0db6f3d8ffc058c29
Ryan Gendel <ryangendel@Ryans-MacBook-Pro.local>
2024-03-28 16:21:40 -0600

    added a universal catch route

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.static(path.join(__dirname, '/public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/", (req, res)=>{
+app.get("*", (req, res)=>{
     res.sendFile(path.join(__dirname,'./public/pages/index.html'));
 })
 
