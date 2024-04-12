ed8cecffa3c401420d083a84b00b47410060b347
Ryan Gendel <ryangendel@Ryans-MacBook-Pro.local>
2024-04-12 13:55:48 -0600

    made it moble responsive

---
+++ NOTES.js
@@ -0,0 +1,217 @@
+  /* PROFILE PICTURES 888888888888888888 */
+  /* @import "compass/css3";
+
+  @import url(https://fonts.googleapis.com/css?family=Open+Sans:400,300,700);
+  
+  .container1 {
+    max-width:500px;
+    margin-left:auto;
+    margin-right:auto;
+    margin-top:10%;
+  }
+  
+  h1 {
+    font-family: "Open Sans", sans;
+    font-weight:300;
+    text-align:center;
+    color:rgba(255,255,255,0.6)
+  }
+  
+  h2 {
+    color:#2c3e52;
+    font-family:  "Open Sans", sans;
+    font-size: 14px;
+    text-align:center;
+    font-weight:700;
+    margin-bottom:50px;
+    
+  }
+  
+  .service-details {
+      width: 460px;
+      height: 250px;	
+      height: 230px;
+      overflow: hidden;
+      position: relative;
+  }
+  
+  .service-details img {
+      position: absolute;
+      top: 0;
+      left: 0;
+      height: inherit;
+      width: auto;
+      height: 100%;
+      float: left;
+      transition: all 0.8s;
+      -moz-transition: all 0.8s;
+  
+  }
+  
+  .service-details:hover img {
+      opacity: 0.4 !important;
+  }
+  
+  .service-details .service-hover-text h3 {
+      padding: 0px;
+      margin: 0px;
+      font-size: 25px;
+    font-weight:300;
+      font-family: "Open Sans";
+  }
+  
+  .service-details .service-hover-text h4 {
+      padding: 0px;
+      padding-bottom: 13px;
+      margin: 0px;
+      font-size: 14px;
+    letter-spacing: 3px;
+      width: 90%;
+      font-family: "Open Sans";
+    text-transform:uppercase;
+      border-bottom: 2px solid #000;
+  }
+  
+  .service-details .service-hover-text p {
+      padding-top: 13px;
+      font-size: 14px;
+      line-height: 20px;
+      font-family: "Open Sans";
+  }
+  
+  
+  .service-details .service-hover-text{
+      width: 42%;
+      height: 89%;
+      position: absolute;
+      top: 0%;
+      left: 50%;
+      padding: 3% 4%;
+      background: black;
+      color: rgba(255,255,255,1);
+      transition: all 0.5s ease-in-out;
+      -moz-transition: all 0.4s;
+  }
+  
+  .service-details:hover .service-hover-text {
+      display: block !important;
+      color: rgba(255,255,255,1);
+      background:#CFB87C;
+      left: 0px;
+      top: 0px;
+  }
+  
+  
+  .service-details .service-text {
+      width: 50%;
+      height: inherit;
+      background: #000;
+      float: left;
+      position: absolute;
+      left: 50%;
+  }
+  
+  .service-details .service-text p {
+      padding:100px 0px 0px 20px;
+      font-size: 24px;
+      font-family: "Open Sans";
+    font-weight:700;
+      color: #fff;
+  }
+  
+  .service-details .service-text p span {
+      font-family: "Open Sans" !important;
+  }
+  
+  .service-details .service-text a , .service-white .service-text {
+      padding: 0px 0px 0px 20px;
+      font-size: 14px !important;
+      color:#CFB87C !important;
+      font-family: "Open Sans" !important;
+      text-decoration: none !important; 
+  }
+  
+  .service-details .service-text {
+      float: left;
+  } 
+  
+  .service-white {
+      background: #eee !important;
+      width: 50% !important;
+      height: inherit !important;
+  }
+  
+  .service-white p {
+      color: #000 !important;
+  } */
+
+
+//   .team-container {
+//     text-align: center;
+//     width: 90%;
+//     max-width: 960px;
+//     margin: 20px;
+// }
+
+// .team-member {
+//     display: flex;
+//     align-items: center;
+//     justify-content: start;
+//     background: #ffffff;
+//     box-shadow: 0 4px 8px rgba(0,0,0,0.1);
+//     margin: 20px;
+//     cursor: pointer;
+//     border-radius: 10px;
+//     overflow: hidden;
+// }
+
+// .team-member img {
+//     width: 150px;
+//     height: 150px;
+//     border-radius: 50%;
+//     margin: 15px;
+// }
+
+// .member-info {
+//     text-align: left;
+//     flex-grow: 1;
+// }
+
+// .member-info h3, .member-info p {
+//     margin: 0;
+// }
+
+// .bio {
+//     display: none;
+//     padding: 10px;
+//     color: #666;
+//     font-size: 14px;
+// }
+
+// @media (max-width: 600px) {
+//     .team-member {
+//         flex-direction: column;
+//         align-items: center;
+//     }
+
+//     .member-info {
+//         text-align: center;
+//     }
+// }
+//     /* PROFILE PICTURES 88888888888888888*/
+
+//     @media screen and (max-width: 24.375em) {
+//         #title {
+//           font-size: 4rem;
+//         }
+//       }
+
+//       @media (max-width: 600px) {
+//         .logo-container {
+//             padding: 10px; /* Reduced padding on mobile */
+//             margin: auto; /* Center-align the container */
+//         }
+//         div.px3{
+//             padding-left:0
+//         }
+//     }
